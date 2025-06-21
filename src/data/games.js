8e6d94932ad8579fc8b17659ed82eb72d476b685
jlanlobao <jlanlobao@gmail.com>
2025-06-21 05:03:53 -0300

    primeiro commit

---
+++ src/data/games.js
@@ -0,0 +1,238 @@
+export const games = [
+    {
+
+        //GAMES DE AÇÂO
+        id: 1, 
+        name: "Marvel's Spider-Man 2",
+        thumb: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2651280/header.jpg?t=1749838789",
+        originalPrice: 249.90,
+        promoPrice: 219.90,
+        ageRating: "livre",
+        rating: 8.0,
+        category: ["Action", "Heros"],
+        photos: [
+            "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2651280/ss_2fb76f7e76dbddf87715d18ae1bcb5cd1dda5747.600x338.jpg?t=1749838789",
+            "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2651280/ss_74e612eb0ae275bd6e7f69d407dda8b03d83629f.600x338.jpg?t=1749838789"
+        ],
+        description: "Be Greater. Together. The incredible power of the symbiote forces Peter Parker and Miles Morales into a desperate fight as they balance their lives, friendships, and their duty to protect in an exciting chapter of the critically acclaimed Spider-Man franchise on PC."
+    },
+
+    {
+        id: 2, 
+        name: "Marvel'Devil May Cry 5",
+        thumb: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/601150/header.jpg?t=1701395090",
+        originalPrice: 99.90,
+        promoPrice: 24.75,
+        ageRating: "livre",
+        rating: 9.5,
+        category: ["Action", "Hack and Slash"],
+        photos: [
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/601150/ss_4ce180ed8979a51c72de51f985e9e9ba13500508.600x338.jpg?t=1701395090",
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/601150/ss_4410bada2565843dae693b03ac3a50256ff5dd66.1920x1080.jpg?t=1701395090"
+        ],
+        description: "The ultimate Devil Hunter is back in style, in the game action fans have been waiting for."
+    },
+    {
+        id: 3, 
+        name: "Rise of the Tomb Raider",
+        thumb: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/391220/header.jpg?t=1729011444",
+        originalPrice: 124.90,
+        promoPrice: 18.74,
+        ageRating: "livre",
+        rating: 7.9,
+        category: ["Action", "Adventure"],
+        photos: [
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/391220/ss_4ef0868ecfc6b19bc1af18b88cabe33fe8147cf7.600x338.jpg?t=1729011444",
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/391220/ss_2b612bfa62d920b061e900e1f7a975a2de6729ec.600x338.jpg?t=1729011444"
+        ],
+        description: "Rise of the Tomb Raider: 20 Year Celebration includes the base game and Season Pass featuring all-new content. Explore Croft Manor in the new “Blood Ties” story, then defend it against a zombie invasion in “Lara’s Nightmare”."
+    },    
+    {
+        id: 4, 
+        name: "Resident Evil 6",
+        thumb: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/221040/header.jpg?t=1700452901",
+        originalPrice: 124.90,
+        promoPrice: 18.74,
+        ageRating: 16,
+        rating: 6.5,
+        category: ["Action", "Zombies"],
+        photos: [
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/221040/ss_3b8620c28828d160fa4ad13456bd6ef71f43cdf3.1920x1080.jpg?t=1700452901",
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/221040/ss_42cfd0ed6ef0bf03a28e48f1faf25c21777d6fa6.600x338.jpg?t=1700452901"
+        ],
+        description: "Blending action and survival horror, Resident Evil 6 promises to be the dramatic horror experience of 2013."
+    },
+    {
+        id: 5, 
+        name: "Lies of P",
+        thumb: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2848330/header.jpg?t=1750144463",
+        originalPrice: 124.90,
+        promoPrice: 124.90,
+        ageRating: 14,
+        rating: 7.5,
+        category: ["Action", "Soulslike"],
+        photos: [
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2848330/ss_4be5866692efa42e936aed0c29e240b991fcd455.600x338.jpg?t=1750144463",
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2848330/e529d06a153d18bfc852982a37e5ff0cff45e143/ss_e529d06a153d18bfc852982a37e5ff0cff45e143.600x338.jpg?t=1750144463"
+        ],
+        description: "Lies of P is a thrilling soulslike that takes the story of Pinocchio, turns it on its head, and sets it against the darkly elegant backdrop of the Belle Epoque era."
+    },
+
+    //GAMES DE SIMULAÇÂO
+    {
+        id: 6, 
+        name: "iRacing",
+        thumb: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/266410/header.jpg?t=1749827136",
+        originalPrice: 124.90,
+        promoPrice: 124.90,
+        ageRating: 14,
+        rating: 5.5,
+        category: ["Simulation", "Racing"],
+        photos: [
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/266410/ss_9c899ec74789641a700cb5e6a209574b1538942f.600x338.jpg?t=1749827136",
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/266410/ss_9892a7414c45f0ca78a2e0c1be73ff663b07c0d7.600x338.jpg?t=1749827136"        
+        ],
+        description: "We are the world’s premier motorsports racing game. An iRacing membership provides entry into the newest form of motorsport: internet racing. Internet racing is a fun, easy, and inexpensive way for race fan and gamers alike to enjoy the thrill of the racetrack from the comfort of their home."
+    },
+    {
+        id: 7, 
+        name: "DRIFT CE",
+        thumb: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/520950/header.jpg?t=1728551332",
+        originalPrice: 99.90,
+        promoPrice: 29.90,
+        ageRating: "livre",
+        rating: 6.5,
+        category: ["Simulation", "Racing"],
+        photos: [
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/520950/ss_5208659c11547158f4ec0953c236ca09084aea1a.1920x1080.jpg?t=1728551332",
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/520950/ss_7e116d859a1b4b25502b3d227920cff759a7f470.1920x1080.jpg?t=1728551332"        
+        ],
+        description: "Tune your dream drift car, change parts, boost performance and challenge other players on Japan's legendary EBISU circuits!"
+    },
+    {
+        id: 8, 
+        name: "F1® 25",
+        thumb: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/3059520/37f833ca5bd3d5c3eec2b411131f3e00f580bbe7/header.jpg?t=1748910595",
+        originalPrice: 249.90,
+        promoPrice: 249.90,
+        ageRating: 10,
+        rating: 8.7,
+        category: ["Simulation", "Racing"],
+        photos: [
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/3059520/41e2dc99ae68ff3f7628403fac109aa9b784fa8b/ss_41e2dc99ae68ff3f7628403fac109aa9b784fa8b.1920x1080.jpg?t=1748910595",
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/3059520/eafccbc66e73d86a7df704d1c767932e884655a3/ss_eafccbc66e73d86a7df704d1c767932e884655a3.600x338.jpg?t=1748910595"        
+        ],
+        description: ""
+    },
+    {
+        id: 9, 
+        name: "Ultimate Fishing Simulator",
+        thumb: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/468920/header.jpg?t=1750160795",
+        originalPrice: 5.90,
+        promoPrice: 59.90,
+        ageRating: "livre",
+        rating: 8.3,
+        category: ["Simulation", "Fishing"],
+        photos: [
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/468920/ss_9f86e7d64571fad9414f9c99c53f89a205918dd3.600x338.jpg?t=1750160795",
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/468920/ss_9f86e7d64571fad9414f9c99c53f89a205918dd3.600x338.jpg?t=1750160795"        
+        ],
+        description: "Leave your mark on the world of racing in F1® 25, the official video game of the 2025 FIA Formula One World Championship™, featuring a revamped My Team mode, the thrilling third chapter of Braking Point, and more!"
+    },
+    {
+        id: 10, 
+        name: "Euro Truck Simulator 2",
+        thumb: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/227300/header.jpg?t=1750168417",
+        originalPrice: 5.90,
+        promoPrice: 59.90,
+        ageRating: "livre",
+        rating: 8.3,
+        category: ["Simulation", "Driving"],
+        photos: [
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/468920/ss_9f86e7d64571fad9414f9c99c53f89a205918dd3.600x338.jpg?t=1750160795",
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/468920/ss_9f86e7d64571fad9414f9c99c53f89a205918dd3.600x338.jpg?t=1750160795"        
+        ],
+        description: "Travel across Europe as king of the road, a trucker who delivers important cargo across impressive distances! With dozens of cities to explore, your endurance, skill and speed will all be pushed to their limits."
+    },
+
+    //GAMES DE MMORPG
+    {
+        id: 11, 
+        name: "Black Desert",
+        thumb: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/582660/header.jpg?t=1749061800",
+        originalPrice: 17.46,
+        promoPrice: 17.46,
+        ageRating: "livre",
+        rating: 6.5,
+        category: ["Simulation", "Driving"],
+        photos: [
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/582660/ss_a4c2348412fec008a7dadacb52793bc2f6185f26.600x338.jpg?t=1749061800",
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/582660/ss_8cd369deed11b84f64e37d08ea8e835be888d3f5.600x338.jpg?t=1749061800"        
+        ],
+        description: "Played by over 20 million Adventurers - Black Desert Online is an open-world, action MMORPG. Experience intense, action-packed combat, battle massive world bosses, fight alongside friends to siege and conquer castles, and train in professions such as fishing, trading, crafting, cooking, and more!"
+    },
+
+    //GAMES DE ESPORTES
+    {
+        id: 12, 
+        name: "EA SPORTS™ Madden NFL 25",
+        thumb: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2582560/header.jpg?t=1749121293",
+        originalPrice: 299.00,
+        promoPrice: 59.80,
+        ageRating: "livre",
+        rating: 6.9,
+        category: ["Sports", "Football (American)"],
+        photos: [
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2582560/ss_5fa710294b51ab3490a1583022c5fa15b2048370.1920x1080.jpg?t=1749121293",
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2582560/ss_8a6e45ae05dd3ed8a8570f67f9cf011e66714450.600x338.jpg?t=1749121293"        
+        ],
+        description: "Hit like you mean it with FieldSENSE™ powered by BOOM Tech. Experience how the new physics-informed dynamic tackling system unlocks realistic collisions & new gameplay animations on both sides of the ball."
+    },
+    {
+        id: 13, 
+        name: "EA SPORTS FC™ 25",
+        thumb: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2669320/ec3fb7747fd8080ef53d7686e0d98c5abe1f51f1/header.jpg?t=1749120687",
+        originalPrice: 59.80,
+        promoPrice: 59.80,
+        ageRating: "livre",
+        rating: 7.0,
+        category: ["Sports", "Football (Soccer)"],
+        photos: [
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2582560/ss_5fa710294b51ab3490a1583022c5fa15b2048370.1920x1080.jpg?t=1749121293",
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2582560/ss_8a6e45ae05dd3ed8a8570f67f9cf011e66714450.600x338.jpg?t=1749121293"        
+        ],
+        description: "EA SPORTS FC™ 25 gives you more ways to win for the club. Team up with friends in your favourite modes with the new 5v5 Rush, and manage your club to victory as FC IQ delivers more tactical control than ever before."
+    },
+    //GAMES DE LUTA
+    {
+        id: 14, 
+        name: "Street Fighter™ 6",
+        thumb: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1364780/header.jpg?t=1749099737",
+        originalPrice: 179.00,
+        promoPrice: 179.00,
+        ageRating: 12,
+        rating: 8.0,
+        category: ["Fight"],
+        photos: [
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1364780/ss_ed46aaa9ed94dd6f35a703070f8df16cee5aef61.600x338.jpg?t=1749099737",
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1364780/ss_2cfc2472e7ff99fd354935135d91e36e81951795.600x338.jpg?t=1749099737"        
+        ],
+        description: "Here comes Capcom’s newest challenger! Street Fighter™ 6 launches worldwide on June 2nd, 2023 and represents the next evolution of the Street Fighter™ series! Street Fighter 6 spans three distinct game modes, including World Tour, Fighting Ground and Battle Hub."
+        },
+    {
+        id: 15, 
+        name: "Mortal Kombat 11",
+        thumb: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/976310/header.jpg?t=1747925751",
+        originalPrice: 22.99,
+        promoPrice: 22.99,
+        ageRating: 18,
+        rating: 8.9,
+        category: ["Fight"],
+        photos: [
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/976310/ss_4fa4fd2ea1b7ff6c6b699dc9eb717986f80845a4.600x338.jpg?t=1747925751",
+            "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/976310/ss_ecddafdaee86bfb66b0a7290dda445f43195b48d.600x338.jpg?t=1747925751"        
+        ],
+        description: "Mortal Kombat is back and better than ever in the next evolution of the iconic franchise."
+    },
+]
+
---
+++ src/main.js
@@ -0,0 +1,98 @@
+import { games } from "./data/games.js";
+
+// SELETORES
+const divRecommended = document.getElementById("recommended");
+//*****
+const divPromoGames = document.getElementById("promo-games");
+const btnAction = document.getElementById("btn-action");
+const divCarouselInner = document.getElementById("carousel-inner");
+
+// FUNCOES
+
+
+// A PARTIR DA BASE DE DADOS CRIAR UM NOVO ARRAY APENAS COM OS JOGOS QUE SAO PROMOCAO
+// DICA => FILTER
+//*****
+const createPromoGames = games.filter(element => (element.promoPrice < element.originalPrice))
+  
+// Eventos
+btnAction.addEventListener("click", () => {
+  window.location.href = "src/pages/game-category";
+});
+
+window.addEventListener("load", () => {
+
+  //CRIA OS ELEMENTOS DO CAROUSEL 
+  games.forEach((element, index) => {
+    const divCarouselItem = document.createElement("div");
+
+    index===0
+      ? divCarouselItem.classList.add("carousel-item", "active")
+      : divCarouselItem.classList.add("carousel-item")
+    
+    divCarouselItem.innerHTML += `
+      <img src="${element.thumb}" class="d-block w-100" alt="${element.name}">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>${element.name}</h5>
+        <p>R$ ${
+          element.originalPrice > element.promoPrice
+            ? element.promoPrice.toFixed(2)
+            : element.originalPrice.toFixed(2)
+        }</p>
+      </div>`
+
+    divCarouselInner.appendChild(divCarouselItem)
+  })
+
+  //*****PROMOÇAO
+  createPromoGames.forEach((element, index) => {
+    divPromoGames.innerHTML += `
+            <div class="col-lg-4">
+                <h4>${element.name}</h4>
+                <img src="${element.thumb}" />
+
+                <p>R$ ${element.promoPrice.toFixed(2)}</p>
+            </div>
+        `;
+  });
+
+  games.forEach((element, index) => {
+    divRecommended.innerHTML += `
+            <div class="col-lg-4">
+                <h4>${element.name}</h4>
+                <img src="${element.thumb}" />
+
+                <p>R$ ${
+                  element.originalPrice > element.promoPrice
+                    ? element.promoPrice.toFixed(2)
+                    : element.originalPrice.toFixed(2)
+                }</p>
+            </div>
+        `;
+  });
+
+});
+
+
+
+/*
+  games.forEach((element, index) => {
+    divCarouselInner.innerHTML += `
+      <div class=${index===0
+            ? "carousel-item active"
+            : "carousel-item"
+      }>
+        <img src="${element.thumb}" class="d-block w-100" alt="${element.name}">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>${element.name}</h5>
+            <p>R$ ${
+              element.originalPrice > element.promoPrice
+                ? element.promoPrice.toFixed(2)
+                : element.originalPrice.toFixed(2)
+            }</p>
+          </div>
+      </div>
+      `;
+  });
+*/
+
