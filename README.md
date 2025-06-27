2066ccd8982d974d6ee1fcda44544ed994407765
jlanlobao <jlanlobao@gmail.com>
2025-06-27 07:40:40 -0300

    elementos criados a partir de newGames

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 [X] Os recomendados serao os jogos com rating igual o maior do que 8.
 [X] O carousel nao precisa ser dinamico. Pode cria-lo no html diretamente. 
 [X] Seletores de todas as categorias dos botoes. 
-[ ] em game-category criar os elementos a partir de newGames
+[X] em game-category criar os elementos a partir de newGames
--- src/pages/game-category/gameCategory.js
+++ src/pages/game-category/gameCategory.js
@@ -1,5 +1,7 @@
 import { games } from "../../data/games.js";
 
+const divNewGame = document.getElementById("new-games");
+
 window.addEventListener("load", () => {
   const url = new URL(window.location.href);
   const category = url.searchParams.get("category");
@@ -8,4 +10,25 @@ window.addEventListener("load", () => {
   console.log(newGames);
 
   // CRIAR ELEMENTOS A PARTIR DE NEW GAMES
+  newGames.forEach((element) => {
+    divNewGame.innerHTML += `
+      <div class="col-lg-4">
+        <img src="${element.thumb}" class="rounded" />
+        <h4>${element.name}</h4>
+                
+        <p>${
+          element.originalPrice > element.promoPrice
+            ? element.promoPrice.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })
+            : element.originalPrice.toLocaleString("pt-BR", {
+              style: "currency",
+                      currency: "BRL",
+                  })
+              }</p>
+            </div>
+        `;
+  });
+
 });
