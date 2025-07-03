0103c1262e189a25422d27a94ed67d5caa764aeb
jlanlobao <jlanlobao@gmail.com>
2025-07-03 20:00:54 -0300

    alterado script da pagina de categorias

--- src/pages/game-category/gameCategory.js
+++ src/pages/game-category/gameCategory.js
@@ -1,6 +1,14 @@
 import { games } from "../../data/games.js";
+import { createNewGame } from "../../scripts/createNewGames.js";
 
 const divNewGame = document.getElementById("new-games");
+const divPromoGames = document.getElementById("promo-games");
+const btnAction = document.getElementById("btn-action");
+const btnSimulation = document.getElementById("btn-simulation");
+const btnMmorpg = document.getElementById("btn-mmorpg");
+const btnSports = document.getElementById("btn-sports");
+const btnFight = document.getElementById("btn-fight");
+
 
 window.addEventListener("load", () => {
   const url = new URL(window.location.href);
@@ -8,32 +16,37 @@ window.addEventListener("load", () => {
   const newGames = games.filter((e) => e.category.includes(category));
   console.log(newGames);
 
+btnAction.addEventListener("click", () => {
+  window.location.href = `/steam-dev/src/pages/game-category/index.html?category=${btnAction.name}`;
+});
+
+btnSimulation.addEventListener("click", () => {
+  window.location.href = `/steam-dev/src/pages/game-category/index.html?category=${btnSimulation.name}`;
+});
+
+btnMmorpg.addEventListener("click", () => {
+  window.location.href = `/steam-dev/src/pages/game-category/index.html?category=${btnMmorpg.name}`;
+});
+
+btnSports.addEventListener("click", () => {
+  window.location.href = `/steam-dev/src/pages/game-category/index.html?category=${btnSports.name}`;
+});
+
+btnFight.addEventListener("click", () => {
+  window.location.href = `/steam-dev/src/pages/game-category/index.html?category=${btnFight.name}`;
+});
+
+window.addEventListener("load", () => {
+  createNewGame(promoGames, divPromoGames);
+  createNewGame(recomendedGames, divRecommended);
+});
+
   //Titulo
   const newTitle = document.createElement("h2");
   newTitle.textContent = `${category}`;
   divNewGame.appendChild(newTitle);
 
-  
-
   // CRIAR ELEMENTOS A PARTIR DE NEW GAMES
-  newGames.forEach((element) => {
-    divNewGame.innerHTML += `
-      <div class="col-lg-4">
-        <img src="${element.thumb}" class="rounded" />
-        <h4>${element.name}</h4>
-                
-        <p>${
-          element.originalPrice > element.promoPrice
-            ? element.promoPrice.toLocaleString("pt-BR", {
-              style: "currency",
-              currency: "BRL",
-            })
-            : element.originalPrice.toLocaleString("pt-BR", {
-              style: "currency",
-                      currency: "BRL",
-                  })
-        }</p>
-      </div>
-      `;
-  });
+  createNewGame(newGames, divNewGame);
+
 });
